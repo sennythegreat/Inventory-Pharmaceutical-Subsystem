e591b191d7ca4f8a8f797aaf000bedc22fd340ae
senkuman <sduro@gbox.adnu.edu.ph>
2026-04-29 15:27:49 +0800

    deleteting some un built feature

---
+++ inventory_pharmaceutical_subsystem/src/app/pages/transactions/page.js
@@ -0,0 +1,12 @@
+"use client";
+
+export default function DispensePage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold text-gray-900">Transaction Logs</h1>
+      <p className="text-sm text-gray-400 mt-2">
+        This page is under construction. You will be able to view transaction logs from here soon.
+      </p>
+    </div>
+  );
+}
